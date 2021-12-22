016429f396921e8b64530fc8aef85aba21fb6de5
Daniel <danielvieira@petroeng.com.br>
2021-12-22 13:34:14 -0300

    Resgatando dados do banco e inserindo no formulário

--- src/components/Banner.vue
+++ src/components/Banner.vue
@@ -5,25 +5,27 @@
 </template>
 
 <script>
-export default {};
+export default {
+  name: "Banner"
+}
 </script>
 
 <style scoped>
-#main-banner {
-  background-image: url("/img/burger.jpg");
-  background-position: 0 -250px;
-  background-size: cover;
-  height: 500px;
-  display: flex;
-  align-items: center;
-  justify-content: flex-start;
-}
+  #main-banner {
+    background-image: url('/img/burger.jpg');
+    background-position: 0 -350px;
+    background-size: cover;
+    height: 700px;
+    display: flex;
+    align-items: center;
+    justify-content: flex-start;
+  }
 
-#main-banner h1 {
-    color: #FFF;
-    text-align: center;
-    font-size: 60px;
-    background-color: #222;
-    padding: 20px 40px;
-}
+  #main-banner h1 {
+      color: #FFF;
+      text-align: center;
+      font-size: 60px;
+      background-color: #222;
+      padding: 20px 40px;
+  }
 </style>
